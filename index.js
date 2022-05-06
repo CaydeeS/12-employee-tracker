0ca2dedc46d52fcfb715089946b5e2b6553f227d
Yed Star <chstokrp@gmail.com>
2022-05-05 22:03:08 -0700

    start func

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
 const mysql = require("mysql");
 const cTable = require('console.table');
 const inquirer = require ('inquirer'); 
+
+const connection = mysql.createConnection(
+    { host: "localhost",
+      port: 3306,
+      user: "root",
+      password: "TheLastJedi",
+      database: "employee_track_db"
+  });
